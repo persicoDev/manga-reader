af7a2cc9b79eaafe92efe367249011b6f0155504
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-06-22 10:12:29 +0200

    adding observables, json server.

--- frontend/src/app/services/manga.service.ts
+++ frontend/src/app/services/manga.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Manga } from 'src/manga';
+
+const httpOption = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MangaService {
+  private apiUrl = 'http://localhost:5000/mangas';
+  
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getMangas(): Observable<Manga[]> {
+    return this.http.get<Manga[]>(this.apiUrl)
+  }
 }
