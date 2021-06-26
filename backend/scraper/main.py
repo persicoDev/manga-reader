5b8ef32ada144f3ce6e5e1c85457398f5b8127be
Alessio <alessiopersichettidev@gmail.com>
2021-06-26 22:41:09 +0200

    making observables working.

--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -1,27 +1,23 @@
 # !/usr/bin/env python
-# import json
 import requests
 from pages import single_manga, json_save
 from bs4 import BeautifulSoup
 
 
 if __name__ == "__main__":
-    manga_save = {}
-    manga_list_save = []
+    manga_save = { }
+    manga_list_save = [ ]
     i = 1
     while True:
         # try:
-        manga_list = BeautifulSoup(requests.get(
-            f'https://www.mangaworld.io/archive?page={ i }').content, 'html.parser')
+        manga_list = BeautifulSoup(requests.get(f'https://www.mangaworld.io/archive?page={ i }').content, 'html.parser')
         for manga in manga_list('div', class_='entry'):
-            json_single_manga = {}
+            json_single_manga = { }
             json_single_manga['title'] = manga.find('a')['title']
             json_single_manga['preview'] = manga.find('img')['src']
             json_single_manga['bookmarked'] = False
             manga_list_save.append(json_single_manga)
-            manga_save = {
-                "mangas": manga_list_save
-            }
+            manga_save = { "mangas": manga_list_save }
             json_save.file_save(manga_save)
             json_single_manga['link'] = manga.find('a')['href']
             single_manga.get_single_manga(link = json_single_manga['link'])
