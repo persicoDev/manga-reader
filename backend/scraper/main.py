037e591e1a8940753e5f2f0a8821e1cb2959671d
Alessio <alessiopersichettidev@gmail.com>
2021-06-26 15:15:32 +0200

    fixed python scraper.

--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -7,25 +7,25 @@ from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
     manga_save = {}
-    manga_list = []
+    manga_list_save = []
     i = 1
     while True:
-        try:
-            manga_list = BeautifulSoup(requests.get(
-                f'https://www.mangaworld.io/archive?page={ i }').content, 'html.parser')
-            for manga in manga_list('div', class_='entry'):
-                json_single_manga = {}
-                json_single_manga['title'] = manga.find('a')['title']
-                json_single_manga['preview'] = manga.find('img')['src']
-                json_single_manga['bookmarked'] = 'false'
-                manga_list.append(json_single_manga)
-                manga_save = {
-                    "mangas": manga_list
-                }
-                json_save.file_save(manga_save)
-                json_single_manga['link'] = manga.find('a')['href']
-                single_manga.get_single_manga(link = json_single_manga['link'])
-            i += 1
-        except:
-            print('scraping finished')
-            break
+        # try:
+        manga_list = BeautifulSoup(requests.get(
+            f'https://www.mangaworld.io/archive?page={ i }').content, 'html.parser')
+        for manga in manga_list('div', class_='entry'):
+            json_single_manga = {}
+            json_single_manga['title'] = manga.find('a')['title']
+            json_single_manga['preview'] = manga.find('img')['src']
+            json_single_manga['bookmarked'] = 'false'
+            manga_list_save.append(json_single_manga)
+            manga_save = {
+                "mangas": manga_list_save
+            }
+            json_save.file_save(manga_save)
+            json_single_manga['link'] = manga.find('a')['href']
+            single_manga.get_single_manga(link = json_single_manga['link'])
+        i += 1
+        # except:
+        #     print('scraping finished')
+            # break
--- backend/scraper/pages/json_save.py
+++ backend/scraper/pages/json_save.py
@@ -2,5 +2,5 @@ import json
 
 
 def file_save(manga_save):
-    with open("backend/db.json", 'f', encoding='utf-8') as f:
+    with open("backend/db.json", 'w', encoding='utf-8') as f:
         json.dump(manga_save, f, ensure_ascii=False, indent=2)
