b08403c5228a51a333e79020c242a2c68223ec5f
Alessio <alessiopersichettidev@gmail.com>
2021-06-28 22:41:45 +0200

    code refactoring.

--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -2,6 +2,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+
 manga_obj = { }
 
 def get_manga(archive_link):
@@ -23,26 +24,26 @@ def get_more_info(manga_link):
     page_content = BeautifulSoup(requests.get(manga_link).content, 'html.parser')
     manga_part = page_content.find('div', class_='chapters-wrapper')
 
-    # try:
-    for manga in manga_part('div', class_='chapter'):
-        manga_url = manga.find('a')['href']
-        manga_list.append(get_single_chapter(manga_url))
-    # except:
-    #     manga_list = 'Chapter not found'
-    #     pass
+    try:
+        for manga in manga_part('div', class_='chapter'):
+            manga_url = manga.find('a')['href']
+            manga_list.append(get_single_chapter(manga_url))
+    except:
+        manga_list = 'Chapter not found'
+        pass
 
     return manga_list
 
 
 def get_single_chapter(manga_url):
-    # try:
-    tag = None
-    soup = BeautifulSoup(requests.get(manga_url).content, 'html.parser')
-    manga_container = soup.find_all(check_img(tag, soup))
-    chapter_index = soup.find("option", {"value": "0"}).text
-    chapter_index = str(chapter_index[2:len(str(chapter_index))])
-    # except:
-        # print('-error-')
+    try:
+        tag = None
+        soup = BeautifulSoup(requests.get(manga_url).content, 'html.parser')
+        manga_container = soup.find_all(check_img(tag, soup))
+        chapter_index = soup.find("option", {"value": "0"}).text
+        chapter_index = str(chapter_index[2:len(str(chapter_index))])
+    except:
+        print('-error-')
     for i in range(int(chapter_index)):
         if i < 10:
             manga_url = manga_url[:-1]
