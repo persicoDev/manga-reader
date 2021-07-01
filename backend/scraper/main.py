90965edf6a5996afa7aec89bb0ea098fdbda0e31
Alessio <alessiopersichettidev@gmail.com>
2021-07-01 13:20:24 +0200

    fixed scraper.

--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -12,34 +12,28 @@ def get_manga(archive_link):
     data = []
 
     for manga in soup('div', class_='entry'):
-        item_data = {}
-        item_data['name'] = manga.find('a')['title']
-        item_data['preview'] = manga.find('img')['src']
-        item_data['bookmarked'] = False
+        manga_data = {}
+        manga_data['name'] = manga.find('a')['title']
+        manga_data['preview'] = manga.find('img')['src']
+        manga_data['bookmarked'] = False
         print('manga:' + manga.find('a')['href'])
-        item_data['link'] = get_more_info(manga_link=manga.find('a')['href'])
-        data.append(item_data)
+        manga_data['link'] = get_single_manga(manga_link=manga.find('a')['href'])
+        data.append(manga_data)
         manga_obj = { 'mangas': data }
 
 
-def get_more_info(manga_link):
+def get_single_manga(manga_link):
     manga_list = []
     page_content = BeautifulSoup(
         requests.get(manga_link).content, 'html.parser')
     manga_part = page_content.find('div', class_='chapters-wrapper')
-
-    try:
-        for manga in manga_part('div', class_='chapter'):
-            manga_url = manga.find('a')['href']
-            if str(manga_url).endswith('?style=list') or str(manga_url).endswith('?style=list'):
-                manga_url = manga_url[:-11]
-            manga_url = f'{ manga_url }/1'
-            print(f'link chapter: { str(manga_url) }')
-            manga_list.append(get_single_chapter(manga_url))
-    except:
-        manga_list = 'Chapter not found'
-        pass
-
+    for manga in manga_part('div', class_='chapter'):
+        manga_url = manga.find('a')['href']
+        if str(manga_url).endswith('?style=list') or str(manga_url).endswith('?style=list'):
+            manga_url = manga_url[:-11]
+        manga_url = f'{ manga_url }/1'
+        print(f'link chapter: { str(manga_url) }')
+        manga_list.append(get_single_chapter(manga_url))
     return manga_list
 
 
@@ -51,23 +45,24 @@ def get_single_chapter(manga_url):
         manga_container = soup.find_all('img', {"class": "img-fluid"})
         chapter_index = soup.find("option", {"value": "0"}).text
         chapter_index = str(chapter_index[2:len(str(chapter_index))])
+        manga_container = manga_container[1]['src']
     except:
         print('-error')
     for i in range(int(chapter_index)):
+        manga_container = manga_container[:-4]
         if i < 10:
-            manga_url = manga_url[:-1]
-            manga_url += str(i + 1)
+            manga_container = manga_container[:-1]
+            manga_container += str(i + 1)
         if i >= 10:
-            manga_url = manga_url[:-2]
-            manga_url += str(i + 1)
+            manga_container = manga_container[:-2]
+            manga_container += str(i + 1)
         if i >= 100:
-            manga_url = manga_url[:-3]
-            manga_url += str(i + 1)
-            manga_url = f'{ manga_url }?style=pages'
-        soup = BeautifulSoup(requests.get(manga_url).content, 'html.parser')
-        manga_container = soup.find_all('img', {"class": "img-fluid"})
-        print('link pages: ' + str(manga_container[1]['src']))
-        save.append(str(manga_container[1]['src']))
+            manga_container = manga_container[:-3]
+            manga_container += str(i + 1)
+
+        manga_container = f'{ manga_container }.jpg'
+        print('link pages: ' + str(manga_container))
+        save.append(str(manga_container))
     return save
 
 
@@ -76,5 +71,5 @@ for i in range(1, 142):
     print(str(i))
     get_manga(link)
 
-with open('backend/db.json', 'w', encoding='utf-8') as f:
+with open('db.json', 'w', encoding='utf-8') as f:
     json.dump(manga_obj, f, ensure_ascii=False, indent=2)
