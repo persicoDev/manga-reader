c08fbdd12ee95db5d9a2052b23637797ebc3ffc2
Alessio <alessiopersichettidev@gmail.com>
2021-07-07 18:37:32 +0200

    bug fix, code refactoring.

--- frontend/src/app/components/read-manga/read-manga.component.ts
+++ frontend/src/app/components/read-manga/read-manga.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Manga } from 'src/manga';
+import { MangaService } from 'src/app/services/manga.service';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-read-manga',
@@ -8,16 +11,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ReadMangaComponent implements OnInit {
 
+  manga!: string;
+  mangas: Manga[] = [];
   id!: number;
 
-  constructor(public activatedRoute:ActivatedRoute) { }
-
-  ngOnInit(): void {
-    this.getManga();
-  }
+  constructor(public activatedRoute:ActivatedRoute, private MangaService: MangaService) { this.MangaService.getMangas().subscribe((mangas) => this.mangas = mangas); }
+ 
+  ngOnInit(): void { this.getManga(); }
 
   getManga() {
-    const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
-    console.log(id);
+    this.id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
   }
 }
