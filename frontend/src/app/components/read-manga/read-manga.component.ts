a45f78d14c7407e4db4e77cf63600173406a310d
Alessio <alessiopersichettidev@gmail.com>
2021-07-07 17:22:40 +0200

    finally added manga details.

--- frontend/src/app/components/read-manga/read-manga.component.ts
+++ frontend/src/app/components/read-manga/read-manga.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-read-manga',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadMangaComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+
+  constructor(public activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getManga();
   }
 
+  getManga() {
+    const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    console.log(id);
+  }
 }
