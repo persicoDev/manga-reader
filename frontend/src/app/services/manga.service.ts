bb4a9af1a8aea1dc3c22b6258975f9b1e55570eb
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-09-06 17:58:58 +0200

    gh pages fix.

--- frontend/src/app/services/manga.service.ts
+++ frontend/src/app/services/manga.service.ts
@@ -14,7 +14,7 @@ const httpOption = {
 
 export class MangaService {
 
-  private apiUrl = 'https://persicodev.github.io/manga-reader/db.json/';
+  private apiUrl = 'https://persicodev.github.io/manga-reader/db.json';
   
   constructor(private http:HttpClient) { }
 
