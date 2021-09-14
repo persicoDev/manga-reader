0253b00ca995892844bab1306c362aa845ae6ce0
Niccolo Fato <niccolofato13@gmail.com>
2021-09-14 10:02:48 +0200

    style changes.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarkedComponent } from './components/bookmarked/bookmarked.component';
 import { HomeComponent } from './components/home/home.component';
 import { ReadMangaComponent } from './components/read-manga/read-manga.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'bookmarked', component: BookmarkedComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'manga/:id', component: ReadMangaComponent }
 ];
