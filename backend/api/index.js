782f4c78a4cee4602450c328f56499522ab3f907
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-10-04 21:30:20 +0200

    heroku fix.

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,7 +10,7 @@ const express = require("express"),
    
 mongoose.Promise = global.Promise;
 
-const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@Manga-Database.sbycl.mongodb.net/Manga-Database?retryWrites=true&w=majority`;
+const url = process.env.MONGO_URL;
 
 mongoose.connect(url, {
       useNewUrlParser: true,
