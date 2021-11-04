332f9ed2f5f0e8579c955613ed9e490463a09170
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-11-04 12:50:17 +0100

    adding user accounts

--- backend/api/routes/manga.routes.js
+++ backend/api/routes/manga.routes.js
@@ -1,23 +1,19 @@
-const express = require('express');
-const mangaRoute = express.Router();
+const router = require('express').Router();
 
 const MangaInfos = require('../models/MangaInfos');
- 
 
-let app = express();
-
-mangaRoute.route('/').get(async (req, res, next) =>{
+router.route('/').get(async (_req, res) =>{
     console.log('performing query')
     res.json(await MangaInfos.find());
 })
 
-mangaRoute.route('/manga/:id').get(async (req, res, next) =>{
+router.route('/manga/:id').get(async (req, res, _next) =>{
    res.json(await MangaInfos.findOne({'id': req.params.id}));
 })
 
-mangaRoute.route('/read/:id/').get(async (req, res, next) =>{
+router.route('/read/:id/').get(async (req, res, _next) =>{
     res.json(await MangaInfos.findOne({'id': req.params.id}));
 })
 
 
-module.exports = mangaRoute;
+module.exports = router;
