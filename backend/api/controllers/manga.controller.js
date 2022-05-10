27b3eed7066ef880a690606128073cb95c872a47
persicoDev <alessiopersichettidev@gmail.com>
2022-05-10 14:22:10 +0200

    added manga controller.

---
+++ backend/api/controllers/manga.controller.js
@@ -0,0 +1,29 @@
+const mangaModel = require('../models/manga.model');
+
+
+module.exports = {
+
+    getAll: async (res) => {
+        try {
+            const mangas = await mangaModel.find();
+            return res.status(201).json(mangas);
+        } catch(err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    getOne: async (req, res) => {
+        try {
+            if (req.params.id === undefined)
+                return res.status(400).json({ message: 'not found'});
+            const manga = await mangaModel.findById({'id': req.params.id});
+            return res.json(manga);
+        } catch(err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+
+}
