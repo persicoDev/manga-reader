84315e7b69d903238b7715b11d70f2f09acc1d9c
persicoDev <alessiopersichettidev@gmail.com>
2022-05-10 14:27:40 +0200

    added route index.

---
+++ backend/api/routes/index.routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router(),
+      mangaRoutes = require('./manga.routes'),
+      userRoutes =  require('./user.routes');
+
+
+router.use('/mangas', mangaRoutes);
+router.use('user', userRoutes);
